0ba3d9fa0ffd2b1a1d74fe4adcabee1e809cc7af
ziaullhaq11 <ziaullhaq11@gmail.com>
2022-01-10 20:36:23 +0530

    notifications are working but for one token only

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect ,useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, Text, View } from 'react-native';
 import * as Notifications from 'expo-notifications'
@@ -17,20 +17,33 @@ Notifications.setNotificationHandler({
 
 export default function App() {
 
+  const [pushToken,setPushToken] = useState()
+
   useEffect(() => {
     //For IOS
     Permissions.getAsync(Permissions.NOTIFICATIONS)
       .then((statusObj) => {
-        if (statusObj.status !== "granted") {
+        if (statusObj.status !== "granted") { //Checking do we have permissions if not then asking permissions via this below code. And then returning it so it will be available for the next then method
           return Permissions.askAsync(Permissions.NOTIFICATIONS);
         }
-        return statusObj; //if we dont pass then we wont get this data below
+        return statusObj; 
       })
       .then((statusObj) => {
         if (statusObj.status !== "granted") { 
-          return;
+          // return; //if we just return the next thenn block will still executes 
+          throw new Error('Permission not granted!')
         }
-      }); 
+      }).then(() => {
+        console.log('Getting token')
+        return Notifications.getExpoPushTokenAsync();
+      }).then(data => {
+        const token = data.data;
+        setPushToken(token)
+      })
+      .catch(err => {
+        console.log(err)
+        return null;
+      })
 
   }, [])
 
@@ -51,14 +64,28 @@ export default function App() {
   }, [])
 
   const triggerNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({ //This is the local notification process
-      content: {
-        title: 'My first local notification',
-        body: " This is my first local notification we are sending",
+    // Notifications.scheduleNotificationAsync({ //This is the local notification process
+    //   content: {
+    //     title: 'My first local notification',
+    //     body: " This is my first local notification we are sending",
+    //   },
+    //   trigger: { //when should notification trigger
+    //     seconds : 10,
+    //   }
+    // })
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: "POST",
+      headers: {
+        'Accept': "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        "Content-Type" : "application/json"
       },
-      trigger: { //when should notification trigger
-        seconds : 10, 
-      }
+      body: JSON.stringify({
+        to: pushToken,
+        data: { extraData: "Some Data" },
+        title: "Sent via the app",
+        body : "This push notification was sent via the app"
+      })
     })
   }
 
